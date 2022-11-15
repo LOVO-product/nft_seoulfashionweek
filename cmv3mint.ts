b38c9772f1801a2456346a742575621e3e322ab2
sewon <sewon@lovo.ai>
2022-11-15 17:07:00 +0900

    remove key from code

--- cmv3mint.ts
+++ cmv3mint.ts
@@ -2,41 +2,45 @@ import { keypairIdentity, Metaplex, toBigNumber, toDateTime } from "@metaplex-fo
 import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js";
 const bs58 = require('bs58');
 const csv = require("csvtojson");
+
 const csvFilePath = "fashion_addr.csv";
+import keyFilePath from './devnetKey.json';
 
 async function main() {
-    const connection = new Connection(clusterApiUrl("devnet"));
+    const connection = new Connection(clusterApiUrl('devnet'));
     const metaplex = new Metaplex(connection);
 
+
     const collectionAuthority = Keypair.fromSecretKey(
-        Uint8Array.from([199, 51, 94, 128, 183, 189, 143, 20, 48, 70, 226, 109, 250, 158, 135, 17, 86, 177, 175, 17, 76, 231, 174, 95, 208, 167, 1, 19, 49, 183, 165, 98, 233, 169, 60, 242, 57, 173, 160, 97, 58, 126, 189, 28, 203, 192, 58, 88, 4, 54, 231, 209, 0, 148, 201, 92, 139, 223, 153, 128, 224, 56, 172, 253])
+        Uint8Array.from(keyFilePath)
     );
     metaplex.use(keypairIdentity(collectionAuthority));
 
 
     const candyMachine = await metaplex.candyMachines().findByAddress({
-        address: new PublicKey("GzwzG6zGBhUSDsB749fP2EZ9xcgpM7Rq1Jw7GZLck7fg")
+        address: new PublicKey("JB9hQZYJgSzvoofNsKhCJGcNbX8E4tZxwvSRFnbypqPC")
     });
 
     const getAddr = "AKtj3WffACYeNmAxgcncuXeX9GRfn9mz5Qnqz4ygTF9b";
+    const getAddr2 = "23Wgj2MWmtc3rBcVFCeM2sVzE4Q32tecE4qnjebznTEB";
+    let addrList = [getAddr, getAddr2, getAddr, getAddr2, getAddr, getAddr2, getAddr, getAddr2, getAddr, getAddr2]; //10개 ㅇㅋ
     // await mintOne("AKtj3WffACYeNmAxgcncuXeX9GRfn9mz5Qnqz4ygTF9b", metaplex, candyMachine, collectionAuthority);
-    // let addrList = [getAddr,getAddr,getAddr,getAddr,getAddr,getAddr,getAddr,getAddr,getAddr,getAddr]; //10개 ㅇㅋ
 
     // 실제로 실행
-    let addrList1 = await makeInfluencers(87,0);
-    let addrList2 = await makeInfluencers(87,1);
-    let addrList3 = await makeInfluencers(87,2);
-    let addrList4 = await makeInfluencers(87,3);
-    let addrList5 = await makeInfluencers(87,4);
-    let addrList6 = await makeInfluencers(87,5);
-    let addrList7 = await makeInfluencers(87,6);
-    let addrList8 = await makeInfluencers(87,7);
-    let addrList9 = await makeInfluencers(87,8);
-    console.log(addrList3);
+    // let addrList1 = await makeInfluencers(87,0);
+    // let addrList2 = await makeInfluencers(87,1);
+    // let addrList3 = await makeInfluencers(87,2);
+    // let addrList4 = await makeInfluencers(87,3);
+    // let addrList5 = await makeInfluencers(87,4);
+    // let addrList6 = await makeInfluencers(87,5);
+    // let addrList7 = await makeInfluencers(87,6);
+    // let addrList8 = await makeInfluencers(87,7);
+    // let addrList9 = await makeInfluencers(87,8);
+    // console.log(addrList3);
     // console.log(addrList2);
 
 
-    // await mintAll(addrList, metaplex, candyMachine, collectionAuthority);
+    await mintAll(addrList, metaplex, candyMachine, collectionAuthority);
 }
 
 const mintAll = async (addrList: string[], metaplex: Metaplex, candyMachine: any, collectionAuthority: Keypair) => {
@@ -64,12 +68,12 @@ const makeInfluencers = async (endCount: number, epoch: number) => {
     const jsonArray = await csv().fromFile(csvFilePath);
     let arr: string[] = [];
 
-    let start = 10*(epoch);
-    let end= start+10;
-    if(end >endCount){
+    let start = 10 * (epoch);
+    let end = start + 10;
+    if (end > endCount) {
         end = endCount
     }
-    
+
     for (let i = start; i < end; i++) {
         arr.push(jsonArray[i].wallet_adr);
     }
--- cmv3setup.ts
+++ cmv3setup.ts
@@ -1,28 +1,29 @@
 import { keypairIdentity, Metaplex, toBigNumber, toDateTime } from "@metaplex-foundation/js";
 import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js";
-// import {loadKeypair} from "utils";
+import keyFilePath from './devnetKey.json';
+
 
 async function main() {
     const connection = new Connection(clusterApiUrl("devnet"));
     const metaplex = new Metaplex(connection);
 
     const collectionAuthority = Keypair.fromSecretKey(
-        Uint8Array.from([199, 51, 94, 128, 183, 189, 143, 20, 48, 70, 226, 109, 250, 158, 135, 17, 86, 177, 175, 17, 76, 231, 174, 95, 208, 167, 1, 19, 49, 183, 165, 98, 233, 169, 60, 242, 57, 173, 160, 97, 58, 126, 189, 28, 203, 192, 58, 88, 4, 54, 231, 209, 0, 148, 201, 92, 139, 223, 153, 128, 224, 56, 172, 253])
+        Uint8Array.from(keyFilePath)
     );
     metaplex.use(keypairIdentity(collectionAuthority));
 
-    //1. 콜렉션 NFT 만들기
-    // const { nft: collectionNft } = await metaplex.nfts().create({
-    //     name: "9SFWL",
-    //     uri: "https://lovo.mypinata.cloud/ipfs/QmSFCb2kF7WLiTfewscXS7qHRgMmTugF8vx7xmnHou4buK",
-    //     sellerFeeBasisPoints: 0,
-    //     isCollection: true,
-    //     updateAuthority: collectionAuthority,
-    // });
-    // console.log(collectionNft.address.toBase58()); 
+    // 1. 콜렉션 NFT 만들기
+    const { nft: collectionNft } = await metaplex.nfts().create({
+        name: "22SFW",
+        uri: "https://lovo.mypinata.cloud/ipfs/QmSFCb2kF7WLiTfewscXS7qHRgMmTugF8vx7xmnHou4buK",
+        sellerFeeBasisPoints: 0,
+        isCollection: true,
+        updateAuthority: collectionAuthority,
+    });
+    console.log(collectionNft.address.toBase58()); 
 
     //2 캔디머신만들기
-    // const collectionNFT = new PublicKey("CdXR7GbpF42K91j1zjytjpNCiiYLM6MN1Xn3Z4xMMqx");
+    // const collectionNFT = new PublicKey("AvZ4AuzpS8fWSHX1oj2szMdLxmn9HbiYTnCGfWubc3qy");
 
     // const { candyMachine } = await metaplex.candyMachines().create({
     //     itemsAvailable: toBigNumber(87), //87개
@@ -51,27 +52,27 @@ async function main() {
 
     ////3. 만들어진 캔디머신 넣기
     let candyMachine = await metaplex.candyMachines().findByAddress({
-        address: new PublicKey("GzwzG6zGBhUSDsB749fP2EZ9xcgpM7Rq1Jw7GZLck7fg")
+        address: new PublicKey("JB9hQZYJgSzvoofNsKhCJGcNbX8E4tZxwvSRFnbypqPC")
     });
 
     console.log(candyMachine.itemsLoaded);
 
     ////4.이제 하나하나 데이터 주입할거임
-    const data :any[]=[]
+    // const data :any[]=[]
 
-    for(let i = 0; i<44; i++){
-        const item = {
-            name:'', uri:''
-        }
-        data.push(item)
-    }
+    // for(let i = 0; i<43; i++){
+    //     const item = {
+    //         name:'', uri:''
+    //     }
+    //     data.push(item)
+    // }
 
-    const out = await metaplex.candyMachines().insertItems({
-        candyMachine,
-        index : 43,
-        items: data,
-    });
-    console.log(out.response.signature);
+    // const out = await metaplex.candyMachines().insertItems({
+    //     candyMachine,
+    //     index : 44,
+    //     items: data,
+    // });
+    // console.log(out.response.signature);
 
     //한번더 확인
     // let candyMachine = await metaplex.candyMachines().findByAddress({
